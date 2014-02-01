ab82db001a1f102d726d815cc52a156d124a49bd
Luke Lee <durdenmisc@gmail.com>
2014-02-01 10:32:39 -0600

    Remove untitled artist/album/tracks and sort by artist them album

--- itunes_parse.py
+++ itunes_parse.py
@@ -26,21 +26,32 @@ class Tag(object):
         self.stream.write('\t%s' % (text))
 
 
+def _track_meta(track):
+    # Get headers for track meta data like name, album, artist, etc.
+    return [t[0].text for t in zip(track[0::2], track[1::2])]
+
+
 def main():
     # Could use argparse, but keep it simple for now
     import sys
     if len(sys.argv) != 3:
-        print 'Usage: <in_file.xml> <out_file.html'
+        print 'Usage: <in_file.xml> <out_file.html>'
         sys.exit(1)
 
     xml = sys.argv[1]
     html = sys.argv[2]
 
+    # FIXME: This uses a lot of memory, maybe we shouldn't read entire file
+    # into memory at once?
     root = et.parse(xml).getroot()
     tracks = root.find('dict').find('dict').findall('dict')
 
+    # Don't show all the columns
+    name_idx = 1
+    end_album_idx = 4
+
     try:
-        headers = [k.text for k in tracks[0].findall('key')]
+        headers = [k.text for k in tracks[0].findall('key')[name_idx:end_album_idx]]
     except IndexError:
         headers = []
 
@@ -62,13 +73,20 @@ def main():
                     with Tag(html_file, '<th>') as th:
                         th.text(header)
 
-            for track in tracks:
-                with Tag(html_file, '<tr>') as tr:
-                    for key, val in zip(track[0::2], track[1::2]):
-                        with Tag(html_file, '<td>') as td:
+            # Sort by artist then album
+            for track in sorted(tracks, key=lambda t: (t[5].text, t[7].text)):
+                # Skip tracks that have missing name, album or artist will be
+                # skipped
+                meta = _track_meta(track)
+                if 'Name' not in meta or 'Album' not in meta or (
+                   'Artist' not in meta):
+                    continue
 
+                with Tag(html_file, '<tr>') as tr:
+                    for item in track[3:8:2]:
+                        with Tag(html_file, '<td>') as td:
                             try:
-                                td.text('%s' % (val.text))
+                                td.text('%s' % (item.text))
                             except UnicodeEncodeError:
                                 td.text('&nbsp;')
 
